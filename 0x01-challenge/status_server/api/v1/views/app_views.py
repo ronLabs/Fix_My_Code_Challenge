52e5caf706a17edf56d0a56feae6470cd4c38851
ronLabs <ron.altamir@outlook.com>
2022-06-03 20:42:57 +0000

    Debugging

---
+++ 0x01-challenge/status_server/api/v1/views/app_views.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Index view
+"""
+from flask import jsonify, Blueprint
+from api.v1.views import app_views
+
+
+app_views = Blueprint('app_views', __name__)
+
+@app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Status of the web server
+    """
+    return jsonify({"status": "OK"})
